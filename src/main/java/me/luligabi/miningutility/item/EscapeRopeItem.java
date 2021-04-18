6e26f3598bf88e5dd7240ac84c05cb278c5ca0e8
Luligabi1 <luligabi111@gmail.com>
2021-04-18 13:39:34 -0300

    Fixed 0,0,0 teleport glitch, added messages for Escape Rope

--- src/main/java/me/luligabi/miningutility/item/EscapeRopeItem.java
+++ src/main/java/me/luligabi/miningutility/item/EscapeRopeItem.java
@@ -4,6 +4,9 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.text.Style;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
@@ -23,28 +26,25 @@ public class EscapeRopeItem extends Item {
         if(!player.isSneaking()) {
             CompoundTag itemCompoundTag = item.getTag();
 
-            try {
-                if(player.getEntityWorld().toString().equals(itemCompoundTag.getString("world"))) {
-                    player.teleport(itemCompoundTag.getDouble("x"),
-                            itemCompoundTag.getDouble("y"),
-                            itemCompoundTag.getDouble("z"));
-                    item.damage(1, player, (entity) -> player.sendToolBreakStatus(player.getActiveHand()));
-                } else {
-                    System.out.println("different dimensions");
-                }
-            } catch(NullPointerException exception) {
-                System.out.println("can't teleport");
+            if(itemCompoundTag.getDouble("x") != 0 && itemCompoundTag.getDouble("y") != 0 && itemCompoundTag.getDouble("z") != 0) { //if coordinates are all zeros, the position wasn't set.
+                player.teleport(itemCompoundTag.getDouble("x"),
+                        itemCompoundTag.getDouble("y"),
+                        itemCompoundTag.getDouble("z"));
+                item.damage(1, player, (entity) -> player.sendToolBreakStatus(player.getActiveHand()));
+            } else {
+                player.sendMessage(new TranslatableText(
+                        "message.miningutility.escape_rope.fail").setStyle(Style.EMPTY.withColor(Formatting.RED)), true);
             }
-
         } else {
             CompoundTag compoundTag = new CompoundTag();
             compoundTag.putDouble("x", player.getX());
             compoundTag.putDouble("y", player.getY());
             compoundTag.putDouble("z", player.getZ());
-            compoundTag.putString("world", player.getEntityWorld().toString());
 
             item.setTag(compoundTag);
-            System.out.println("setting tags");
+            player.sendMessage(new TranslatableText(
+                    "message.miningutility.escape_rope.setteleport", player.getX(), player.getY(), player.getZ())
+                    .setStyle(Style.EMPTY.withColor(Formatting.YELLOW)), true);
         }
         return TypedActionResult.success(item);
     }
