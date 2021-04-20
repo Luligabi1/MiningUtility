0a3a3304a1a65183364824a9bca565c95055516f
Luligabi1 <luligabi111@gmail.com>
2021-04-20 17:23:06 -0300

    Removed Tunnel Excavator and magna lib

--- src/main/java/me/luligabi/miningutility/registry/ItemRegistry.java
+++ src/main/java/me/luligabi/miningutility/registry/ItemRegistry.java
@@ -2,7 +2,6 @@ package me.luligabi.miningutility.registry;
 
 import me.luligabi.miningutility.MiningUtility;
 import me.luligabi.miningutility.item.EscapeRopeItem;
-import me.luligabi.miningutility.item.TunnelExcavatorItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -11,10 +10,8 @@ public class ItemRegistry {
 
     public static void register() {
         Registry.register(Registry.ITEM, new Identifier(MiningUtility.MOD_ID, "escape_rope"), ESCAPE_ROPE_ITEM);
-        Registry.register(Registry.ITEM, new Identifier(MiningUtility.MOD_ID, "tunnel_excavator"), TUNNEL_EXCAVATOR_ITEM);
     }
 
     public static EscapeRopeItem ESCAPE_ROPE_ITEM = new EscapeRopeItem(new FabricItemSettings().group(MiningUtility.ITEM_GROUP).maxDamage(16));
 
-    public static TunnelExcavatorItem TUNNEL_EXCAVATOR_ITEM = new TunnelExcavatorItem(new FabricItemSettings().group(MiningUtility.ITEM_GROUP).maxCount(16));
 }
