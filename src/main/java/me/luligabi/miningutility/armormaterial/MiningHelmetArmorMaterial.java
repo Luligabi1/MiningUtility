206f484ea5d26bb77d75edb1e77608907f686f29
Luligabi1 <luligabi111@gmail.com>
2021-04-21 21:24:08 -0300

    Added Mining Helmet (WIP)

---
+++ src/main/java/me/luligabi/miningutility/armormaterial/MiningHelmetArmorMaterial.java
@@ -0,0 +1,50 @@
+package me.luligabi.miningutility.armormaterial;
+
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+
+public class MiningHelmetArmorMaterial implements ArmorMaterial {
+
+    @Override
+    public int getDurability(EquipmentSlot slot) {
+        return 15;
+    }
+
+    @Override
+    public int getProtectionAmount(EquipmentSlot slot) {
+        return 2;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 0;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return SoundEvents.ITEM_ARMOR_EQUIP_GENERIC;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return "mining_helmet";
+    }
+
+    @Override
+    public float getToughness() {
+        return 0;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return 0;
+    }
+}
--- src/main/java/me/luligabi/miningutility/registry/ItemRegistry.java
+++ src/main/java/me/luligabi/miningutility/registry/ItemRegistry.java
@@ -1,10 +1,11 @@
 package me.luligabi.miningutility.registry;
 
 import me.luligabi.miningutility.MiningUtility;
+import me.luligabi.miningutility.armormaterial.MiningHelmetArmorMaterial;
 import me.luligabi.miningutility.item.EscapeRopeItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.WallStandingBlockItem;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -12,12 +13,14 @@ public class ItemRegistry {
 
     public static void register() {
         Registry.register(Registry.ITEM, new Identifier(MiningUtility.MOD_ID, "escape_rope"), ESCAPE_ROPE_ITEM);
-
         Registry.register(Registry.ITEM, new Identifier(MiningUtility.MOD_ID, "mini_torch"), MINI_TORCH);
+        Registry.register(Registry.ITEM, new Identifier(MiningUtility.MOD_ID, "mining_helmet"), MINING_HELMET);
     }
 
     public static EscapeRopeItem ESCAPE_ROPE_ITEM = new EscapeRopeItem(new FabricItemSettings().group(MiningUtility.ITEM_GROUP).maxDamage(16));
 
     public static BlockItem MINI_TORCH = new WallStandingBlockItem(BlockRegistry.MINI_TORCH_BLOCK, BlockRegistry.WALL_MINI_TORCH_BLOCK, new FabricItemSettings().group(MiningUtility.ITEM_GROUP));
 
+    public static final ArmorMaterial MINING_HELMET_ARMOR_MATERIAL = new MiningHelmetArmorMaterial();
+    public static final Item MINING_HELMET = new ArmorItem(MINING_HELMET_ARMOR_MATERIAL, EquipmentSlot.HEAD, new Item.Settings().group(MiningUtility.ITEM_GROUP));
 }
